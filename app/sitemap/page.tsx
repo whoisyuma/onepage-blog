ac28c91599f780ec0784105fe4a2f94227ba1442
whoisyuma <whoisyuma.0913@gmail.com>
2025-08-07 13:05:32 +0900

    fix sitemap

--- app/sitemap/page.tsx
+++ app/sitemap/page.tsx
@@ -1,5 +1,6 @@
 import { client } from "@/libs/microcms"
 import Link from "next/link"
+import HomeProfile from "../components/HomeProfile";
 
 // ISR設定
 export const revalidate = 300;
@@ -55,44 +56,39 @@ export default async function SiteMap() {
     const tags = await getTag();
 
     return (
-        <div className='bg-white pt-18 min-h-screen pb-10'>
-            <header className="py-12 border-b border-gray-200">
-                <div className="container mx-auto px-4 md:px-8">
-                    <h1 className="text-4xl md:text-5xl font-bold tracking-tight text-gray-900">
-                        サイトマップ
-                    </h1>
-                </div>
-            </header>
+      <main className='bg-white pt-18 min-h-screen pb-10'>
+        <div className="flex flex-col md:flex-row gap-2 md:gap-6 md:mt-10 max-w-6xl md:mx-auto">
+          <aside className="md:w-3/10 w-full md:p-0 p-4">
+            <HomeProfile/>
+          </aside>
 
-            <div className="container mx-auto px-4 md:px-8 py-12 grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-16">
-                <section className="p-5">
-                    <h2 className="text-2xl font-bold mb-4 text-gray-800">固定ページ</h2>
-                    <ul className="space-y-2 text-lg ml-5">
-                        <li className="list-disc"><Link href='/' className="text-gray-600 hover:text-gray-900 hover:border-b">ホーム</Link></li>
-                        {/* <li className="list-disc"><Link href='/' className="text-gray-600 hover:text-gray-900 hover:border-b">お問い合わせ</Link></li>
-                        <li className="list-disc"><Link href='/' className="text-gray-600 hover:text-gray-900 hover:border-b">プライバシーポリシー</Link></li> */}
-                    </ul>
-                </section>
+          <section className="md:w-7/10 w-full md:px-4 px-2">
+            <div className="mb-5">
+              <h2 className="text-2xl font-bold mb-4 text-gray-800 bg-gray-200 p-2">固定ページ</h2>
+              <ul className="space-y-2 text-lg ml-10">
+                <li className="list-disc"><Link href='/' className="text-gray-600 hover:text-gray-900 hover:border-b">ホーム</Link></li>
+              </ul>
+            </div>
 
-                <section className="p-5">
-                    <h2 className="text-2xl font-bold mb-4 text-gray-800">カテゴリ一覧</h2>
-                    {categories && categories.length > 0 && (
-                        <ul className="space-y-2 text-lg ml-5">
-                            {categories.map((category) => (
-                                <li key={category.id} className="list-disc">
-                                    <Link href={`categories/${category.slug}`} className="text-gray-600 hover:text-gray-900 hover:border-b">
-                                        {category.name}
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                </section>
-
-                <section className="p-5">
-                    <h2 className="text-2xl font-bold mb-4 text-gray-800">タグ一覧</h2>
+            <div className="mb-5">
+              <h2 className="text-2xl font-bold mb-4 text-gray-800 bg-gray-200 p-2">カテゴリ一覧</h2>
+                {categories && categories.length > 0 && (
+                  <ul className="space-y-2 text-lg ml-10">
+                    {categories.map((category) => (
+                      <li key={category.id} className="list-disc">
+                        <Link href={`categories/${category.slug}`} className="text-gray-600 hover:text-gray-900 hover:border-b">
+                          {category.name}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+            </div>
+              
+            <div>
+              <h2 className="text-2xl font-bold mb-4 text-gray-800 bg-gray-200 p-2">タグ一覧</h2>
                     {tags && tags.length > 0 && (
-                        <ul className="flex flex-wrap mt-1">
+                        <ul className="flex flex-wrap mt-1 ml-5">
                             {tags.map((tag) => (
                                 <li key={tag.id}>
                                     <Link href={`/tags/${tag.slug}`} className="inline-flex items-center bg-white hover:bg-gray-100 border mr-1 mb-1 border-gray-400 rounded-xl px-3 py-1 text-sm text-gray-600 hover:border-gray-500 transition duration-200">
@@ -102,8 +98,9 @@ export default async function SiteMap() {
                             ))}
                         </ul>
                     )}
-                </section>
             </div>
+          </section>
         </div>
+      </main>
     )
 }
