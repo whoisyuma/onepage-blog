5d444e4514727f8501317d78c663e22859b2e176
whoisyuma <whoisyuma.0913@gmail.com>
2025-08-19 13:16:11 +0900

    fix: iframe UI

--- app/components/BlogContent.tsx
+++ app/components/BlogContent.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react';
+import style from "./BlogContent.module.css";
 
 // Propsの型指定
 type Props = {
@@ -15,11 +16,32 @@ export default function BlogContent({content, className}: Props) {
         setIsClient(true);
     }, [])
 
+
+    useEffect(() => {
+        if (!isClient) return;
+
+        const container = document.querySelector(`.${className}`);
+        const iframes = container?.querySelectorAll('iframe');
+
+        iframes?.forEach((iframe) => {
+            const src = iframe.getAttribute('src') || '';
+
+            if (src.includes('google.com/maps')) {
+                const parentDiv = iframe.parentElement;
+                if (parentDiv && parentDiv.tagName === 'DIV') {
+                    parentDiv.classList.add(style.googlemapWrapper);
+                }
+            }
+        })
+    }, [isClient, className]);
+
     if (!isClient) {
         return null;
     }
 
     return (
-        <div className={className} dangerouslySetInnerHTML={{ __html: content}} />
+        <div className={className}>
+            <div dangerouslySetInnerHTML={{ __html: content}} />
+        </div>
     )
 }
